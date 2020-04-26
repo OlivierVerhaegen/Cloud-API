4bb5181a49edf73f9fb114c6ccdf34302869c809
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2020-04-26 10:09:10 +0200

    Added database initialization

---
+++ Cloud-API/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Cloud_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cloud_API
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+
+        public DbSet<BodyPart> BodyParts { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Cloud-API/Startup.cs
+++ Cloud-API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,9 +23,24 @@ namespace Cloud_API
 
         public IConfiguration Configuration { get; }
 
+        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddDebug());
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(
+                options => options
+                .UseLoggerFactory(loggerFactory)
+                .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            // De validatie zal gebeuren maar er wordt niet direct een error gegeven eerst wordt de action in de controller aangeroepen.
+            // Nu moeten we wel zelf de modelstate checken in de controller.
+            //services.Configure<ApiBehaviorOptions>(options =>
+            //{
+            //    options.SuppressModelStateInvalidFilter = true;
+            //});
+
             services.AddControllers();
         }
 
